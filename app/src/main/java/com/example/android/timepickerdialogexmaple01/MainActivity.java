ac58282499d500c1edf345b1427f7fe930d0aa60
kc07wlaf <kc.goal@gmail.com>
2018-01-26 02:20:33 +0800

    TimePickerDialog example 01 - uses onClick method (on a button) - includes bug fix for Nougat, which does not show the TimePickerDialog in spinner mode

---
+++ app/src/main/java/com/example/android/timepickerdialogexmaple01/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.android.timepickerdialogexmaple01;
+
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public String selectedTime;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void showTimePickerDialog(View v) {
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "timePicker");
+    }
+
+}
---
+++ app/src/main/java/com/example/android/timepickerdialogexmaple01/TimePickerFragment.java
@@ -0,0 +1,63 @@
+package com.example.android.timepickerdialogexmaple01;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.text.format.DateFormat;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+
+/**
+ * Created by kcc on 26-Jan-18.
+ */
+
+public class TimePickerFragment extends DialogFragment
+        implements TimePickerDialog.OnTimeSetListener {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the current time as the default values for the picker
+        final Calendar c = Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+
+        // Create a new instance of TimePickerDialog and return it
+        return new TimePickerDialog(getActivity(), AlertDialog.THEME_HOLO_LIGHT, this, hour, minute,
+                DateFormat.is24HourFormat(getActivity()));
+        //this is Android Studio's original TimePickerDialog, which contains a bug-- it does not show
+        //the spinner, which should be used in the THEME_HOLO_LIGHT and THEME_HOLO_Dark
+        //other default themes include THEME_DEVICE_DEFAULT_DARK, THEME_DEVICE_DEFAULT_LIGHT, THEME_TRADITIONAL
+
+        //return new TimePickerDialogFixedNougatSpinner(getActivity(), AlertDialog.THEME_HOLO_LIGHT, this, hour, minute,
+        //        DateFormat.is24HourFormat(getActivity()));
+        //the above is the TimePickerDialog with the fix
+    }
+
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        // Do something with the time chosen by the user
+
+        int selectedHour = hourOfDay;
+        int selectedMinute = minute;
+        String selectedAM_PM;
+
+        if (selectedHour<=11){selectedAM_PM="AM";}
+        else {
+            selectedAM_PM = "PM";
+            if (selectedHour>12){selectedHour=selectedHour-12;}
+        }
+
+        TextView textViewDisplayTime=(TextView) getActivity().findViewById(R.id.textView);
+        textViewDisplayTime.setText(Integer.toString(selectedHour)+":"+Integer.toString(selectedMinute)+" "+selectedAM_PM);
+
+        TextView tv1=(TextView) getActivity().findViewById(R.id.textView1);
+        tv1.setText("Hour: "+view.getCurrentHour()+" Minute: "+view.getCurrentMinute());
+        //getCurrentHour() is a TimePicker built-in method; deprecated in API23; use getHout() instead
+        ///However, because I am using minimum API version number as 15, thus using getHour would
+        //cause an error, though the error could be surpressed
+        //tv1.setText("Hour: "+view.getHour()+" Minute: "+view.getMinute());
+    }
+}
